6867104030e38885332af56cdf740ac26bebecef
Benjamin Mikiten <benjaminmikiten@gmail.com>
2019-08-24 12:06:44 -0500

    add benjaminmikiten userspace files

---
+++ users/benjaminmikiten/benjaminmikiten.h
@@ -0,0 +1,9 @@
+#ifndef USERSPACE
+#define USERSPACE
+
+#include "quantum.h"
+
+#define SCN_SHT    LCTL(LSFT(LGUI(KC_4)))	// Take a screenshot (MacOS)
+#define SCN_LCK    LCTL(LGUI(KC_Q))	// Lock screen (MacOS)
+
+#endif
