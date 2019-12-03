f327ee7b66fe2214580aad0924215a027a4c9e08
Benjamin Mikiten <benjaminmikiten@gmail.com>
2019-12-02 19:22:40 -0600

    userspace work

---
+++ users/benjaminmikiten/benjaminmikiten.h
@@ -0,0 +1,45 @@
+// #ifndef USERSPACE
+// #define USERSPACE
+
+#include "quantum.h"
+
+
+#define QWERTY_LH_ALPHA \
+// #ifndef USERSPACE
+// #define USERSPACE
+
+#include "quantum.h"
+
+// RIGHT HAND ROWS
+#define LH_R0 \
+   KC_1, KC_2, KC_3, KC_4, KC_5
+
+#define QWERTY_LH_R1 \
+   KC_Q, KC_W, KC_E, KC_R, KC_T
+
+#define QWERTY_LH_R2 \
+   KC_A, KC_S, KC_D, KC_F, KC_G
+
+#define QWERTY_LH_R3 \
+   KC_Z, KC_X, KC_C, KC_V, KC_B
+
+
+#define LH_R4 \
+   KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_ENT
+
+
+// LEFT HAND ROWS
+#define RH_R0 \
+   KC_6, KC_7, KC_8, KC_9, KC_0
+
+#define QWERTY_RH_R1 \
+   KC_Y, KC_U, KC_I, KC_O, KC_P
+
+#define QWERTY_RH_R2 \
+   KC_H, KC_J, KC_K, KC_L, KC_SCLN
+
+#define QWERTY_RH_R3 \
+   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH
+
+#define RH_R4 \
+   KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
