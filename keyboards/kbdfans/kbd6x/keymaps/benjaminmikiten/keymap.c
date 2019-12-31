abc66b34aa9dd5fceb091c21c4c3dc6bb278cabe
Benjamin Mikiten <benjamin.m@mc-j.com>
2019-12-31 10:36:24 -0600

    formatted hhkb

--- keyboards/kbdfans/kbd6x/keymaps/benjaminmikiten/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/benjaminmikiten/keymap.c
@@ -13,36 +13,64 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include "benjaminmikiten.h"
 #include QMK_KEYBOARD_H
+
 
 // stock hhkb as referenced by https://i.imgur.com/QoBTDHf.png
 
+enum hhkb_keycodes {
+  QWERTY = SAFE_RANGE,
+  RAISE,
+  LOWER,
+  ADJUST
+}
 
+// EXTRA _______ ON EITHER SIDE OF THE BOTTOM ROW IS NECESSARY
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-      KC_TRNS, KC_LALT, KC_LGUI,                   KC_SPACE,                 KC_RGUI, KC_RALT, KC_TRNS
-      ),
+  [_QWERTY] = LAYOUT_hhkb_wrapper(
+      KC_ESC,  ________________NUMBER_LEFT________________, ________________NUMBER_RIGHT_______________, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+      KC_TAB,  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_LBRC, KC_RBRC, KC_BSPC,
+      KC_LCTL, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_QUOT, KC_ENT,
+      KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, KC_RSFT, MO(RAISE),
+      _______, KC_LALT, KC_LGUI,                             KC_SPACE,                                   KC_RGUI, KC_RALT, _______
+  ),
 
-  [1] = LAYOUT(
-      KC_,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_DEL,
-      KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, RESET,
-      KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_MSTP, KC_TRNS, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_PLUS, KC_UNDS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
-      ),
+  [_DVORAK] = LAYOUT_hhkb_wrapper(
+      KC_ESC,  ________________NUMBER_LEFT________________, ________________NUMBER_RIGHT_______________, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+      KC_TAB,  _________________DVORAK_L1_________________, _________________DVORAK_R1_________________, KC_LBRC, KC_RBRC, KC_BSPC,
+      KC_LCTL, _________________DVORAK_L2_________________, _________________DVORAK_R2_________________, KC_QUOT, KC_ENT,
+      KC_LSFT, _________________DVORAK_L3_________________, _________________DVORAK_R3_________________, KC_RSFT, MO(RAISE),
+      _______, KC_LALT, KC_LGUI,                             KC_SPACE,                                   KC_RGUI, KC_RALT, _______
+  ),
 
-  [2] = LAYOUT(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_DEL,
-      KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, RESET,
-      KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_MSTP, KC_TRNS, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_PLUS, KC_UNDS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
-      ),
+  [_RAISE] = LAYOUT_hhkb_wrapper(
+      RESET,  _________________FUNC_LEFT_________________, _________________FUNC_RIGHT________________, KC_F11,  KC_12,   KC_INS, KC_DEL,
+      KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,   _______, _______,
+      _______, KC_VOLU, KC_VOLD, KC_MUTE, KC_MSTP, _______, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______,
+      _______, _______, _______, _______, _______, _______, KC_PLUS, KC_UNDS, KC_END, KC_PGDN, KC_DOWN, _______, _______,
+      _______, _______, _______,                            _______,                                     _______, _______, _______
+  ),
+
+  [_LOWER] = LAYOUT_hhkb_wrapper(
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______,                            _______,                                     _______, _______, _______
+  ),
+
+  [_ADJUST] = LAYOUT_hhkb_wrapper(
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______,                            _______,                                     _______, _______, _______
+  ),
+
+
 };
 
 
@@ -56,5 +84,40 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode){
+      case QWERTY:
+        if(record -> event.pressed){
+          set_single_persistent_default_layer(_QWERTY)
+        }
+        return false;
+        break;
+      case DVORAK:
+        if(record->event.pressed){
+          set_single_persistent_default_layer(_DVORAK)
+        }
+        return false;
+        break;
+      case LOWER:
+        if (record->event.pressed) {
+          layer_on(_LOWER);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+          layer_off(_LOWER);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+      case RAISE:
+        if (record->event.pressed) {
+          layer_on(_RAISE);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+          layer_off(_RAISE);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
+
+  }
   return true;
 }
