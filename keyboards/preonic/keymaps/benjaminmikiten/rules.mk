101147de3ae812b11f6c44f5df74ee8ab73a3f20
Benjamin Mikiten <benjamin.m@mc-j.com>
2020-06-16 11:32:44 -0500

    remove key delay changes

--- keyboards/preonic/keymaps/benjaminmikiten/rules.mk
+++ keyboards/preonic/keymaps/benjaminmikiten/rules.mk
@@ -3,5 +3,5 @@ SRC += muse.c
 BOOTMAGIC_ENABLE = lite
 ENCODER_ENABLE = no
 # Audio AND RGB can't  both  be enabled.
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = yes
+AUDIO_ENABLE = yes
+RGBLIGHT_ENABLE = no
