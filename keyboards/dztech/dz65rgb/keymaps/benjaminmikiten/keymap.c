29c65e828faa16ecdbf0a74f86cfce717895556e
Benjamin Mikiten <benjaminmikiten@gmail.com>
2020-09-06 22:34:32 -0500

    65 work

--- keyboards/dztech/dz65rgb/keymaps/benjaminmikiten/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/benjaminmikiten/keymap.c
@@ -3,24 +3,47 @@
 
 enum dz65rgb_keycodes {
     QWERTY = SAFE_RANGE,
-    RAISE
+    RAISE,
+    LOWER,
+    ADJUST
 };
 
-#define M_RAISE MO(_RAISE)
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+#define ADJUST MO(_ADJUST)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_65_ansi_wrapper(
-        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL,        KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RALT, RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_GESC, ________________NUMBER_LEFT________________, ________________NUMBER_RIGHT_______________, KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
+        KC_TAB,  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_LBRC, KC_RBRC, KC_BSLS, KC_MUTE,
+        KC_LCTL, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_QUOT,          KC_ENT,  KC_VOLU,
+        KC_LSFT,          _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, KC_RSFT, KC_UP,   KC_VOLD,
+        LOWER,   KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RALT, RAISE,   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_RAISE] = LAYOUT_65_ansi_wrapper(
-        KC_GESC,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-        _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,   KC_PGUP,
-        _______,        RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
-        KC_LSFT,                 _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
-        _______,        _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+        _______, _________________FUNC_LEFT_________________, _________________FUNC_RIGHT________________, KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
+        _______, ____________________RGB0___________________, _______, _______, _____FUNCTION_BLOCK_0____, _______, _______, _______, KC_PGUP,
+        _______, _____________HHKB_MEDIA___________, _______, _______, _______, _____FUNCTION_BLOCK_1____, _______,          _______, KC_PGDN,
+        KC_LSFT,          _______, _______, _______, _______, _______, _______, _____FUNCTION_BLOCK_2____, _______, _______, KC_VOLU, KC_MUTE,
+        RESET,   _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+    ),
+    [_LOWER] = LAYOUT_65_ansi_wrapper(
+       KC_GESC, _________________FUNC_LEFT_________________, _________________FUNC_RIGHT________________, KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
+       _______, ____________________RGB0___________________, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
+       _______, ____________________RGB1___________________, _______, _____________VIM_ARROWS___________, _______,          _______, KC_PGDN,
+       KC_LSFT,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
+       RESET,   _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+    ),
+    [_ADJUST] = LAYOUT_65_ansi_wrapper(
+       KC_GESC, _________________FUNC_LEFT_________________, _________________FUNC_RIGHT________________, KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
+       _______, ____________________RGB0___________________, _______, _______, _____FUNCTION_BLOCK_0____, _______, _______, _______, KC_PGUP,
+       _______, ____________________RGB1___________________, _______, _______, _____FUNCTION_BLOCK_1____, _______,          _______, KC_PGDN,
+       KC_LSFT,          _______, _______, _______, _______, _______, _______, _____FUNCTION_BLOCK_2____, _______, _______, KC_VOLU, KC_MUTE,
+       RESET,   _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
